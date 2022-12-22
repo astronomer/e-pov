faeb7816b2a248d7b696cb878209c563b1fa1ca6
Matthew Ray <matt.ray@astronomer.io>
2022-12-22 15:20:53 -0600

    added code samples

--- dags/EY_extraction_demo.py
+++ dags/EY_extraction_demo.py
@@ -1,9 +1,32 @@
+#Just throwing some code in here. Has not been wired to work. DAG load will fail and this may not even be the best way to do it. 
+
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.empty import EmptyOperator
 from datetime import datetime, timedelta 
 
 from airflow.providers.microsoft.azure.operators.asb import AzureServiceBusSendMessageOperator #import error
+
+def _copy_files_to_wasb(self, copy_files: list[CopyFile], **kwargs) -> list[str]:
+        """Upload a list of files from sftp_files to Azure Blob Storage with a new Blob Name."""
+        uploaded_files = []
+        wasb_hook = WasbHook(wasb_conn_id=self.wasb_conn_id)
+        for file in copy_files:
+            with NamedTemporaryFile("w") as tmp:
+                self.sftp_hook.retrieve_file(file.sftp_file_path, tmp.name) #change to wasb get_file
+                self.log.info(
+                    "Uploading %s to wasb://%s as %s",
+                    file.sftp_file_path,
+                    self.container_name,
+                    file.blob_name,
+                )
+                wasb_hook.load_file(
+                    tmp.name,
+                    self.container_name,
+                    file.blob_name,
+                    self.create_container,
+                    **self.load_options,
+                )
 
 dag_owner = 'Astronomer'
 
@@ -34,10 +57,28 @@ with DAG(dag_id='extraction_pipeline',
         
         #Connect using keyvault value of sftp creds and downlod sftp files into elz location
         #https://registry.astronomer.io/providers/microsoft-azure/modules/sftptowasboperator
-        SftpConnector = EmptyOperator(task_id='SftpConnector')
+        SftpConnector = SFTPToWasbOperator(
+            task_id="SftpConnector",
+            # SFTP args
+            sftp_conn_id="sftp_docker",
+            sftp_source_path=f"/home/fe_shepard/{exec_date}",
+            # AZURE args
+            container_name=AZURE_CONTAINER_NAME,
+            blob_prefix=f"raw/{exec_date}/",
+            wasb_conn_id="wasb_docker",
+            wasb_overwrite_object=True,
+            on_failure_callback=task_failure_slack_alert,
+            on_success_callback=task_success_slack_alert,
+            on_retry_callback=task_retry_slack_alert)
 
         #Ingestion pipeline - Move elz to ilz
-        MoveELZtoILZ = EmptyOperator(task_id='MoveELZtoILZ')
+        MoveELZtoILZ = PythonOperator(
+                task_id='MoveELZtoILZ',
+                python_callable = _copy_files_to_wasb,
+                op_kwargs={
+                        #Add args here
+                }
+        )
 
         #Push message to Azure Service Bus
         #https://airflow.apache.org/docs/apache-airflow-providers-microsoft-azure/5.0.1/operators/asb.html#send-message-to-azure-service-bus-queue
