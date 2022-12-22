1470799087fc7fff58ef5a271fcfdd37df5f18c7
Matthew Ray <matt.ray@astronomer.io>
2022-12-22 10:43:59 -0600

    sample dag framework

---
+++ dags/extraction.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime, timedelta 
+
+dag_owner = 'Astronomer'
+
+default_args = {'owner': dag_owner,
+        'depends_on_past': False,
+        'retries': 2,
+        'retry_delay': timedelta(minutes=5)
+        }
+
+with DAG(dag_id='extraction_pipeline',
+        default_args=default_args,
+        description='',
+        start_date=datetime(),
+        schedule_interval='',
+        catchup=False,
+        tags=['']
+):
+
+        #Fetch metadata from control table, get schedule
+        #This should probably run as a seperate pipeline that dynamically creates DAGs
+        tdf_dhb_data_extration = EmptyOperator(task_id='tdf_dhb_data_extration')
+
+        #Validate connections, trigger sftpconnector pipeline
+        tdf_dhb_data_extraction_portal_sftp = EmptyOperator('tdf_dhb_data_extraction_portal_sftp')
+
+        #Once SFTP pipeline is succeeded update status in extraction audit table
+        tdf_dhb_data_extraction_check_status = EmptyOperator('tdf_dhb_data_extraction_check_status')
+        
+        #Connect using keyvault value of sftp creds and downlod sft files into elz location
+        SftpConnector = EmptyOperator('SftpConnector')
+
+        #Ingestion pipeline - Move elz to ilz
+        MoveELZtoILZ = EmptyOperator('MoveELZtoILZ')
+
+        #Push message to Azure Service Bus
+        PushASB = EmptyOperator('PushASB')
+
+        tdf_dhb_data_extration >> tdf_dhb_data_extraction_portal_sftp >> SftpConnector >> [MoveELZtoILZ, tdf_dhb_data_extraction_check_status] >> PushASB
